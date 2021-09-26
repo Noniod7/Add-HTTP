6649becdead8879b245f6f044fbc83b31393363f
Noniod7 <41842911+Noniod7@users.noreply.github.com>
2021-09-27 03:30:26 +0700

    Update add-http.py

--- add-http.py
+++ add-http.py
@@ -1,31 +1,19 @@
-#Powered By : Noniod7
-
 banner = """ 
-
-Mass Add HTTP by Noniod7
-
+Mass Add HTTP | Noniod7
 """
-
 print banner
 
 def http(url):
-
-	try:		https = ('http://'+url)
-
+	try:
+		https = ('http://'+url)
 		open('http.txt','a').write(https+'\n'); print(https)
-
 	except: pass
 
+
 site = raw_input('List Site : ')
-
 ht = open(site, 'r').readlines()
-
 for i in ht:
-
 	try:
-
 		siten = i.strip()
-
 		data=http(siten)
-
 	except: pass
